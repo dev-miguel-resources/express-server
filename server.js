fbdba5c31eba82fb4b434887c8624f34f8d8e751
dev-miguel-resources <ingenieromiguelch@gmail.com>
2022-01-28 12:02:34 -0300

    configuration complete server express

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const connectDB = require("./database");
+const morgan = require("morgan");
+//const bodyParser = require("body-parser");
+const cors = require("cors");
+const { readdirSync } = require("fs");
+require("dotenv").config();
+
+// app middleware express
+const app = express();
+
+// db
+connectDB();
+
+// middlewares
+app.use(morgan("dev"));
+//app.use(bodyParser.json({ limit: "2mb" })); // deprecated = > v14.x.9 
+app.use(express.json({ limit: "2mb" })); // standard configuration
+app.use(cors());
+
+// port
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
