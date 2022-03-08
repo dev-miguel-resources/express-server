865880b34980cddeee64c55981d974b7cdecea8a
dev-miguel-resources <ingenieromiguelch@gmail.com>
2022-03-08 20:13:28 -0300

    products middlewares controller

--- routes/product.js
+++ routes/product.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const { create, listAll } = require("../controllers/product");
+const { create, listAll, productsCount, removeSoft, read, update, list } = require("../controllers/product");
 
 // routes
 router.post("/product", create);
 router.get("/products/:count", listAll);
-/*router.get("/products/total", productsCount);
+router.get("/products/total", productsCount);
 router.patch("/product/:slug", removeSoft);
 router.get("/product/:slug", read);
 router.put("/product/:slug", update);
-router.post("/products", list);*/
+router.post("/products", list);
 
 module.exports = router;
 
